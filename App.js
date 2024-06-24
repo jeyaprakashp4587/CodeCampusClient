3580f83221e40ee475b63f310a585a79e0d7605e
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-06-24 19:42:46 +0530

    after setting

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>hii</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
